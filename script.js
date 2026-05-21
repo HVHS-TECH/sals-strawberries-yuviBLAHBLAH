e3023f30314e617839a8a13fec504c9cbcf3c2dc
Your Name <23096yb@hvhs.school.nz>
2026-05-22 09:58:22 +1200

    i hate ts

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 
 console.log("Running Sal's Strawberries")
 
-function writeForm(){
+function writeForm() {
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
 }
-var userName = prompt("please enter your name. "+name)
-var favFood = prompt("jello "+name+", what is your favourite food?")
+var userName = prompt("please enter your name. " + name)
+var favFood = prompt("jello " + name + ", what is your favourite food?")
 console.log(userName + " likes " + favFood)
+
+function helloWorld() {
+    console.log("Running helloWorld()")
+    firebase.database().ref('/').set(
+        {
+            message: 'works'
+        }
+    )
+}
+
+function byeWorld() {
+    console.log("Running byeWorld()")
+    firebase.database().ref('/').set(
+        {
+            message: 'WORKS.'
+        }
+    )
+}
+
+function list() {
+    console.log("Running list()")
+    firebase.database().ref('/').set(
+        {
+            sector1: {
+                consumers: {
+                }
+            }
+        }
+    )
+}
+function makeDude() {
+    console.log("Running make dude()")
+    let user = "mr.dude";
+    let score = 0;
+    firebase.database().ref('/sector1/consumers/' + user).set(
+        score
+    );
+}
